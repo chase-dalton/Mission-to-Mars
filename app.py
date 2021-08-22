4aea6ea4ddd5b39945d6bb74bac26c508d98a597
Chase Dalton <chase.ryan.dalton@gmail.com>
2021-08-22 12:03:10 -0500

    functional app, scraping, HTML needs work

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
+   #return "Scraping successful!"
 
 if __name__ == "__main__":
    app.run()
